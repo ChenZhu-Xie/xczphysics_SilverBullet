7147c885331df2b01dfdd9213b234e32f59a93e3
Xcz <294302704@qq.com>
2025-10-23 22:43:48 +0800

    Snapshot

--- _plug/page-meta.plug.js
+++
@@ -1,9 +0,0 @@
-export async function indexPageMeta({ name }, index) {
-  const meta = await space.readPageMeta(name);
-  if (!meta) return;
-  index({
-    key: name,
-    tag: "page-meta",
-    value: { lastOpened: meta.lastOpened }
-  });
-}
